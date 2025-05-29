80e9f08404ae9bce40803cc2ba8982243e41ac08
VRASHABHPATIL <vrashabhpatil047@gmail.com>
2025-05-29 13:24:23 +0530

    Update DeleteUserModal.jsx

--- frontend/src/components/modals/DeleteUserModal.jsx
+++ frontend/src/components/modals/DeleteUserModal.jsx
@@ -24,7 +24,7 @@ function DeleteUserModal() {
       //delete req to the server with data payload
       const res = await axios.request({
         method: "delete",
-        url: "http://localhost:3000",
+        url: "http://13.201.68.248:3000/",
         data: { id }
       });
 
